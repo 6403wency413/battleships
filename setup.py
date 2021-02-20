061bc1e1884e96e49990f0555789ab72cb083d66
erwinsalubre <erwin@salubre.ca>
2021-02-20 12:31:37 -0700

    package creation update

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup()
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pygame"]
+
+setup(
+    name="battleships",
+    version="0.0.1",
+    author="Erwin Salubre",
+    author_email="erwin@salubre.ca",
+    description="Battleships game for coding challenge at Xerris",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_package/homepage/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
